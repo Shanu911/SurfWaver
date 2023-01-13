f0e664a9a5347623278f2ac674f05b4e3e8d32cb
Shanu Biswas <76697973+Shanu911@users.noreply.github.com>
2023-01-13 22:37:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,12 +18,12 @@ setup(
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
     python_requires=REQUIRES_PYTHON,
-    py_modules=["surfwaver", "maswGui", "threads","surfwaver\res\mkcmpcc.py","surfwaver\res\mkcmp.py","surfwaver\res\gather_plot.py", "surfwaver\res\aquGeomPlot.py","surfwaver/modswpro/cmpccworkflows.py", "surfwaver/modswpro/modwavefieldtransforms.py","surfwaver/modswpro/modsensor1C.py", "surfwaver/modswpro/modmasw.py", "surfwaver/modswpro/modarray1D.py" ],             # Name of the python package
+    py_modules=["surfwaver", "maswGui", "threads", "res.mkcmpcc", "res.mkcmp","res.gather_plot", "res.aquGeomPlot","modswpro.cmpccworkflows", "modswpro.modwavefieldtransforms","modswpro.modsensor1C", "modswpro.modmasw", "modswpro.modarray1D" ],             # Name of the python package
     package_dir={'':'surfwaver'},     # Directory of the source code of the package
     install_requires=["obspy", "PyQt5", "swprocess", "numpy", "scipy", "matplotlib"],
     keywords=['MASW', 'Multichannel Analysis of Surface Waves', 'Dispersion', 'Inversion', 'seismic', 'active and passive surface wave', "SurfWaveR" ],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Education",
         "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
